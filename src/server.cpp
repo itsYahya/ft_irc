088fc38133af986583552ba5a38c083a35058e7c
yel-mrab <yahya181elmrabti@gmail.com>
2023-02-16 02:04:55 +0100

    feat: getters and setters and a some code was added to the default constructor

--- src/server.cpp
+++ src/server.cpp
@@ -1,9 +1,25 @@
 #include "server.hpp"
 
 server::server(){
-
+	sock = 0;
+	port = 0;
+	FD_ZERO(&s_read);
+	len = sizeof(addr);
+	(void) buffer;
 }
 
 server::~server(){
 	
+}
+
+int		&server::getport(){
+	return (port);
+}
+
+void	server::setpassword(std::string pass){
+	password = pass;
+}
+
+std::string	&server::getpassword(){
+	return (password);
 }
