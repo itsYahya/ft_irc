0909cfe58f836d40b2d3fd56c280500ddd359505
yel-mrab <yahya181elmrabti@gmail.com>
2023-02-16 02:02:33 +0100

    feat: adding some properties to the server class and some functions

--- inc/classes/server.hpp
+++ inc/classes/server.hpp
@@ -2,16 +2,31 @@
 #define SERVER_HPP
 
 #include <iostream>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define BUFFER_SIZE 1024
+#define MAX_FDS 1024
 
 class server{
 	private:
+		int					sock;
+		int					port;
+		std::string			password;
+		fd_set				s_read;
+		struct sockaddr_in	addr;
+		socklen_t			len;
+		char				buffer[BUFFER_SIZE];
 
 	public:
-		int				port;
-		std::string		password;
-		
 		server();
 		~server();
+		void		create();
+		int			&getport();
+		void		setpassword(std::string pass);
+		std::string	&getpassword();
+
 };
 
 #endif
