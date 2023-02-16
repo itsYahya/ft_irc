bc7aef36837a4fc8a19ed6d581079a47ca2d2119
yel-mrab <yahya181elmrabti@gmail.com>
2023-02-16 20:42:05 +0100

    fix: deleting the mini client prototype

--- inc/classes/server.hpp
+++ inc/classes/server.hpp
@@ -11,19 +11,6 @@
 #define BUFFER_SIZE 1024
 #define MAX_FDS 1024
 
-typedef enum e_type{
-	FDFREE = 0,
-	FDBUSY = 1
-}	t_type;
-
-class client{
-	public :
-		t_type	type;
-		client(){type = FDFREE;}
-		void 	reset(){type = FDFREE;}
-		bool	isfree(){return (type == FDFREE);}
-};
-
 class server{
 	private:
 		int					sock;
