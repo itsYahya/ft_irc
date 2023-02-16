efce88e44d9e43ff03f643dcf5463fe80854bd50
yel-mrab <yahya181elmrabti@gmail.com>
2023-02-16 03:01:09 +0100

    feat: function server create was added to the server class

--- src/server.cpp
+++ src/server.cpp
@@ -6,6 +6,7 @@ server::server(){
 	FD_ZERO(&s_read);
 	len = sizeof(addr);
 	(void) buffer;
+	clients.resize(MAX_FDS);
 }
 
 server::~server(){
@@ -22,4 +23,28 @@ void	server::setpassword(std::string pass){
 
 std::string	&server::getpassword(){
 	return (password);
+}
+
+void	server::create(){
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0){
+		std::cerr << "something went wrong !!" << std::endl;
+		exit(1);
+	}
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	if (bind(sock, (struct sockaddr*)&addr, len) < 0){
+		std::cerr << "port in use !!" << std::endl;
+		exit(1);
+	}
+	if (listen(sock, MAX_FDS) < 0){
+		std::cerr << "something went wrong !!" << std::endl;
+		exit(1);
+	}
+	std::cout << "server was created successfully!!" << std::endl;
+}
+
+void	server::close(){
+	::close(sock);
 }
