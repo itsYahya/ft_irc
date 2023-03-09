855b1598db2fa47103be2553caba6206bed6d529
yel-mrab <yahya181elmrabti@gmail.com>
2023-03-09 04:49:27 +0100

    fix: code message that get send with the bot messages so the client can understand it

--- src/bothandler.cpp
+++ src/bothandler.cpp
@@ -1,33 +1,32 @@
 #include "command.hpp"
 #include "helper.hpp"
 
-
 std::string	command::botList(client &c){
-	std::string info = ":localhost " + helper::itos(321);
-	info += " " + c.getnickName() + " BOT :Commands to use with it. (BOT CMD)\n";
-	info += ":localhost " + helper::itos(322) + " " + c.getnickName() + " HELP :List of all the commands available in the server\n";
-	info += ":localhost " + helper::itos(322) + " " + c.getnickName() + " SESS :The time you spend on the server\n";
-	info += ":localhost " + helper::itos(322) + " " + c.getnickName() + " CHECKNICK :Check if a given nick name is free\n";
-	info += ":localhost " + helper::itos(322) + " " + c.getnickName() + " FAV :Add a favorite list of friends\n";
-	info += ":localhost " + helper::itos(323) + " " + c.getnickName() + " End of /LIST\n";
+	std::string info = "\n300 * :";
+	info += c.getnickName() + " :BOT :Commands to use with it. (BOT CMD)\n";
+	info += "300 * :HELP :List of all the commands available in the server\n";
+	info += "300 * :SESS :The time you spend on the server\n";
+	info += "300 * :CHECKNICK :Check if a given nick name is free\n";
+	info += "300 * :FAV :Add a favorite list of friends\n\n";
+	info += "300 * :BOT :" + c.getnickName() + " End of /LIST\n";
 	return (info);
 }
 
 std::string	command::cmdList(client &c){
-	std::string	info = ":localhost " + helper::itos(321);
-	info += " " + c.getnickName() + " HELP :Commands available on this IRC server\n";
-	info += ":localhost " + helper::itos(322) + " " + c.getnickName() + " PRIVMSG\n";
-	info += ":localhost " + helper::itos(322) + " " + c.getnickName() + " JOIN\n";
-	info += ":localhost " + helper::itos(322) + " " + c.getnickName() + " PART\n";
-	info += ":localhost " + helper::itos(322) + " " + c.getnickName() + " LIST\n";
-	info += ":localhost " + helper::itos(322) + " " + c.getnickName() + " KICK\n";
-	info += ":localhost " + helper::itos(322) + " " + c.getnickName() + " QUIT\n";
-	info += ":localhost " + helper::itos(323) + " " + c.getnickName() + " End of /LIST\n";
+	std::string	info = "300 * :";
+	info +=  c.getnickName() + " :BOT :HELP :Commands available on this IRC server\n";
+	info += "300 * :PRIVMSG\n";
+	info += "300 * :JOIN\n";
+	info += "300 * :PART\n";
+	info += "300 * :LIST\n";
+	info += "300 * :KICK\n";
+	info += "300 * :QUIT\n\n";
+	info += "300 * :HELP :" + c.getnickName() + " End of /LIST\n";
 	return (info);
 }
 
 std::string	command::sessionTime(client &c, int fd){
-	std::string msg = "300 * :BOT " + c.getnickName() + " :Your log time on the server is : ";
+	std::string msg = "300 * :" + c.getnickName() + " :BOT :Your log time on the server is : ";
 	msg += helper::timeToString(c.getSessionTime()) + "\n";
 	::send(fd, msg.c_str(), msg.length(), 0);
 	return (msg);
