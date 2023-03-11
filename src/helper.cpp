02f3e884999ec9f3024a6f792f666ccf18b3813a
yel-mrab <yahya181elmrabti@gmail.com>
2023-03-12 00:13:49 +0100

    fix: splitcmds fucntion removing the \n from the end of the str

--- src/helper.cpp
+++ src/helper.cpp
@@ -82,6 +82,7 @@ std::vector<std::string>	helper::splitCmds(std::string buffer, char sep){
 
 	while (std::getline(stream, string, sep)){
 		if (string.front() == '\n') string.erase(string.begin());
+		if (string.size() > 1 && string.back() == '\n') string.erase(string.end() - 1);
 		if (!string.empty()){
 			res.push_back(string);
 		}
