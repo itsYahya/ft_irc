e4b4edc889505623c0c5ae7905da4afd89d2e865
yel-mrab <yahya181elmrabti@gmail.com>
2023-03-13 05:56:19 +0100

    feat: topic cmd handling all the cases

--- src/command.cpp
+++ src/command.cpp
@@ -130,7 +130,33 @@ void	command::topicInform(client &c, const std::string &ch, const std::string &t
 		::send(it->second, msg.c_str(), msg.length(), 0);
 }
 
+void	command::topiCmd(client &c, int fd){
+	std::vector<std::string>	res = helper::split_(body.c_str(), ' ');
+	dbManager::iterator_channel	iter;
+	t_mode						md;
 
+	if (res.size() == 0){
+		sendErrMsg(fd, c.getnickName(), name, " :Not enough parameters\r\n", " 331 ");
+		return ;
+	}
+	iter = dbManager::searchChannel(res[0]);
+	if (dbManager::isEndChannelIter(iter))
+		sendErrMsg(fd, c.getnickName(), res[0], " :No such channel\r\n", " 403 ");
+	else if (res.size() == 1)
+		sendErrMsg(fd, c.getnickName(), res[0], " :No topic is set.\r\n", " 331 ");
+	else{
+		md = c.getmode(res[0]);
+		if (md == NONE)
+			sendErrMsg(fd, c.getnickName(), res[0], " :You're not on that channel\r\n", " 442 ");
+		else if (md == SM_CLIENT)
+			sendErrMsg(fd, c.getnickName(), res[0], " :You're not an operator on that channel\r\n", " 442 ");
+		else{
+			if (res[1].front() == ':') res[1].erase(res[1].begin());
+			iter->second.seTopic(res[1]);
+			topicInform(c, res[0], res[1], iter);
+		}
+	}
+}
 
 void	command::switch_cmd(int fd, dbManager	*db, client &c, std::vector<client> &cls)
 {
